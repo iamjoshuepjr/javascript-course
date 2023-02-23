231b8e6e9653542e8eee145d294d62e48e8e54a6
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-23 14:26:54 -0500

    JavaScript Fundamentals

--- 1-fundamentals/2-operators/1-arithmeticOperators.js
+++ 1-fundamentals/2-operators/1-arithmeticOperators.js
@@ -3,34 +3,60 @@
  *  author: Joshuép Jr.
  */
 
-// ================= ARITHMETIC OPERATORS ========================= 
-/* Arithmetic operators are used to perform arithmetic calculations 
-   Arithmetical operators accepts numerical values as operamds and 
-   returns a single numeical value. (Literals or variables)
+/**
+ * ===================================================================
+ *                       ARITHMETIC OPERATORS 
+ * Arithmetic operators are used to perform arithmetic calculations 
+ * Arithmetical operators accepts numerical values as operamds and 
+ * returns a single numeical value. (Literals or variables)
 */
 
 let a = 24, b = 19;
 
-// ========== Addition Operator (+) ============ 
+/**
+ * =======================
+ *  Addition Operator (+)
+ */
+
 let c = a + b;
 console.log("Addition: " + c);
 
-// ======== Subtraction Operator (-) ===========
+/**
+ * ==========================
+ *  Subtraction Operator (-)
+ */
+
 c = a - b;
 console.log("Substraction: " + c);
 
-// ======== Multiplication Operator (*) ========
+/**
+ * =============================
+ *  Multiplication Operator (*)
+ */
+
 c = a * b;
 console.log("Multiplication: " + c);
 
-// ============ Divide Operator (/) ============ 
+/**
+ * =====================
+ *  Divide Operator (/)
+ */
+
 c = a / b;
 console.log("Division: " + c);
 
-// ========== Remainder Operator (%) =========== 
+/**
+ * ========================
+ *  Remainder Operator (%)
+ */
+
 c = a % b;
 console.log("Remainder: " + c);
 
-// ====== Exponentiation Operator (**)  ======== 
+/**
+ * ==============================
+ *  Exponentiation Operator (**)
+ */
+
 c = a ** c;
 console.log("Power: " + c)
